1c8519e0a4b2e3a62d94cbeabb075fe7fcab7fa3
Estanislau Filho <estanislau.sena@gmail.com>
2018-12-24 14:21:52 -0200

    Projeção de retângulos na cor verde em cada face detectada na imagem em escala cinza

--- face_detect_image.py
+++ face_detect_image.py
@@ -7,3 +7,7 @@ image = cv2.imread("Imagens/scientists.jpg")
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
 faces_detect = face_classifier.detectMultiScale(image_gray)
+
+while(True):
+    for(x, y, width, height) in faces_detect:
+        cv2.rectangle(image, (x, y), (x + width, y + height), (0, 255, 0), 2)
