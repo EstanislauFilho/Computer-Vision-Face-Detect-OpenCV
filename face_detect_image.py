9c1d7e94916bc9d9fb49ef877947c0125e0c60d2
Estanislau Filho <estanislau.sena@gmail.com>
2018-12-24 14:16:31 -0200

    Criação do vetor com as faces detectadas pela função detectMultiScale

--- face_detect_image.py
+++ face_detect_image.py
@@ -5,3 +5,5 @@ face_classifier = cv2.CascadeClassifier("Classificador/haarcascade_frontalface_d
 image = cv2.imread("Imagens/scientists.jpg")
 
 image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+faces_detect = face_classifier.detectMultiScale(image_gray)
