dcdcce174b5fda57dd81b348f78251d246c1db18
Estanislau Filho <estanislau.sena@gmail.com>
2018-12-24 14:13:44 -0200

    Conversão da imagem para escala de cinza

--- face_detect_image.py
+++ face_detect_image.py
@@ -3,3 +3,5 @@ import cv2
 face_classifier = cv2.CascadeClassifier("Classificador/haarcascade_frontalface_default.xml")
 
 image = cv2.imread("Imagens/scientists.jpg")
+
+image_gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
