11413fd22eddcc1920aa049b93fba2572991ce1a
Estanislau Filho <estanislau.sena@gmail.com>
2018-12-26 11:38:15 -0200

    Faces detectadas demarcadas com a função cv2.rectangle

--- face_detect_webcam.py
+++ face_detect_webcam.py
@@ -9,7 +9,10 @@ while(True):
 
     image_video_gray = cv2.cvtColor(image_video, cv2.COLOR_BGR2GRAY)
 
-    facesDetectadas = face_detect.detectMultiScale(image_video_gray, scaleFactor=1.5, minSize=(100, 100))
+    faces_video = face_detect.detectMultiScale(image_video_gray, scaleFactor=1.5, minSize=(100, 100))
+
+    for (x, y, width, heigth) in faces_video:
+        cv2.rectangle(image_video, (x, y), (x + width, y + heigth), (0, 255, 0), 2)
 
     cv2.imshow("Captura Video funcionando", image_video)
     cv2.waitKey(1)
