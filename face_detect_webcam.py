dc32d737f0a2c79af6a895526d516036a2ef96fa
Estanislau Filho <estanislau.sena@gmail.com>
2018-12-26 10:29:58 -0200

    Verificação e leitura da webcam

--- face_detect_webcam.py
+++ face_detect_webcam.py
@@ -3,3 +3,6 @@ import cv2
 face_detect = cv2.CascadeClassifier("Classificador/haarcascade_frontal_default.xml")
 
 video = cv2.VideoCapture(0)
+
+while(True):
+    conect, image_video = video.read()
